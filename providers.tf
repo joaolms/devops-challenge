80b570687ef374688ca0488a9ecdb4a364f51e00
João Luiz Messias <joaolmss@gmail.com>
2023-05-19 16:06:54 -0300

    Remove s3 backend

--- providers.tf
+++ providers.tf
@@ -5,16 +5,9 @@ terraform {
       version = "~> 4.67.0"
     }
   }
-
-  backend "s3" {
-    encrypt              = true
-    region               = "sa-east-1"
-    bucket               = "galp-terraform-state"
-    key                  = "terraform.tfstate"
-    dynamodb_table       = "galp-terraform-state-lock"
-  }
 }
 
 provider "aws" {
   region  = var.region
+  profile = "personal"
 }
