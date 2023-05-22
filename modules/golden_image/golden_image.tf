04397eb95bd7a3481bc331f424c0c384017c2cd2
João Luiz Messias <joaolmss@gmail.com>
2023-05-22 01:47:15 -0300

    correção arquivo

--- modules/golden_image/golden_image.tf
+++ modules/golden_image/golden_image.tf
@@ -1,7 +1,3 @@
-locals {
-  default_content = ""
-}
-
 data "local_file" "packer_manifest" {
-  filename = fileexists("${var.manifest_path}/packer-manifest.json") ? file("${var.manifest_path}/packer-manifest.json") : local.default_content
+  filename = "${var.manifest_path}/packer-manifest.json"
 }
